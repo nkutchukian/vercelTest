565917b567b047d87ae5c53639faa6de1c97358d
Nicolas Kutchukian <nicolas.kutchukian@gmail.com>
2025-07-10 19:21:17 -0400

    Test with React

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Xena from './assets/Xena.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World!</h1>
+          <img src={Xena} alt="React logo" />
+      </div>
+        <p>My Cat</p>
+    </>
+  )
+}
+
+export default App
